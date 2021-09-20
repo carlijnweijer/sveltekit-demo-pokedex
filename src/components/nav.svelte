b7701542dc4d55fb0a3b97d04e82837c339d44db
carlijnweijer <carlijnweijer@gmail.com>
2021-09-20 21:35:34 +0200

    style: :lipstick: Style navbar

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,4 +1,4 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
+<nav class="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+	<a class="btn btn-sm btn-ghost" role="button" href="/">Home</a>
+	<a class="btn btn-sm btn-ghost" role="button" href="/about">About</a>
 </nav>
